9dff3ae4a0747da5924f8cc69267ba1e0349e17c
ArvikVan <arvikvan@gmail.com>
2021-01-16 16:35:25 +0300

    16:35

---
+++ src/com/company/bases/SymbolsTypes.java
@@ -0,0 +1,18 @@
+package com.company.types;
+
+public class SymbolsTypes {
+    public static void main(String[] args) {
+        char a = 102;
+        System.out.println(a);
+        char a2 ='\u0066';//16-ричная форма записи
+        System.out.println(a2);
+
+        String s = "Hello";
+        String s2 = "Hello\nWoooorld";// \n каретка переводящая на друггую строку
+        System.out.println(s);
+        System.out.println(s2);
+        String s3 = "Вставай, проклятьем заклейменый, \n" +
+                "весь мир, голодных и рабов";// в java15 появилась возможность многострочного текста
+        System.out.println(s3);
+    }
+}
