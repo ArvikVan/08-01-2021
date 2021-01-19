737f5fcb44226905939daeb0e907aac338720a26
ArvikVan <arvikvan@gmail.com>
2021-01-20 00:52:11 +0300

    ExCycles 00:51 20-01-2021

---
+++ src/com/company/bases/cycles/ex2Cycle.java
@@ -0,0 +1,21 @@
+package com.company.bases.cycles;
+
+import javax.imageio.plugins.tiff.TIFFImageReadParam;
+
+public class ex2Cycle {
+    public static void main(String[] args) {
+        int popu = 1000000;
+        int born = 14;
+        int death = 8;
+        for (int i = 1; i < 10; i++){
+            if (born > 7) born = born - 1;
+            System.out.println(born);
+            if (death > 6) death = death - 1;
+            System.out.println(death);
+            int diff = born - death;
+            System.out.println(diff);
+            popu = popu + (popu * diff) / 1000;
+            System.out.println("Население в " + i + " год " + popu);
+        }
+    }
+}
---
+++ src/com/company/bases/cycles/ex3Cycle.java
@@ -0,0 +1,20 @@
+package com.company.bases.cycles;
+
+import java.util.Scanner;
+
+public class ex3Cycle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите сумму вклада: ");
+        float sumvklad = scanner.nextFloat();
+        System.out.print("Введите срок вклада: ");
+        int time = scanner.nextInt();
+        float prib = 0;
+        for (int i = 1; i <= time; i++) {
+            sumvklad += sumvklad * 0.07;
+            //sumvklad = sumvklad + sumvklad * 0.07; не работает, требует приведения к типу float
+        }
+        System.out.println("После " + time + " месяцев сумма вклада составит " + sumvklad);
+
+    }
+}
