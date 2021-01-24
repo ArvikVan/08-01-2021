8de84f710d5beb243bfdb6ca355e86913440187b
ArvikVan <arvikvan@gmail.com>
2021-01-25 01:07:42 +0300

    Arrays 01:07 25-01-2021

---
+++ src/com/company/bases/arrays/arrStair.java
@@ -0,0 +1,25 @@
+package com.company.bases.arrays;
+
+public class arrStair {
+    public static void main(String[] args) {
+        int arr[][] = new int[4][];
+        arr[0] = new int[1];
+        arr[1] = new int[2];
+        arr[2] = new int[3];
+        arr[3] = new int[4];
+
+        int k = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = k;
+                k++;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
