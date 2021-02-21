b91f29c766c4ea3506f458ab2b23c7056c710e6c
ArvikVan <arvikvan@gmail.com>
2021-02-21 10:39:30 +0300

    21-02-2021

---
+++ src/com/company/condition/InfinityLoop.java
@@ -0,0 +1,10 @@
+package com.company.condition;
+
+public class InfinityLoop {
+    public static void main(String[] args) {
+        for (int index = 0; index < 10; index++) {
+            System.out.println("infinity" + index);
+            index = index - 1;
+        }
+    }
+}
---
+++ src/com/company/condition/PrintNToM.java
@@ -0,0 +1,13 @@
+package com.company.condition;
+
+public class PrintNToM {
+    public static void out(int n, int m) {
+        for (int i = n; i < m; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintNToM.out(0, 5);
+    }
+}
