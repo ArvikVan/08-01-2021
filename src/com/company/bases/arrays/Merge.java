db2f044a7aed01b8c2c102dfad6775aca1877934
ArvikVan <arvikvan@gmail.com>
2021-03-01 22:50:50 +0300

    01-03-2021

--- src/com/company/bases/arrays/Merge.java
+++ src/com/company/bases/arrays/Merge.java
@@ -1,9 +1,31 @@
 package com.company.bases.arrays;
+
+import java.awt.datatransfer.SystemFlavorMap;
+
 // 3. Объединить два отсортированных массива
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-
+        int count = 0, countLeft = 0, countRight = 0;
+        while (count < rsl.length) {
+            if (left.length == 0) {
+                rsl = right;
+                break;
+            }
+            if (right.length == 0) {
+                rsl = left;
+            }
+            rsl[count] = (left[countLeft] < right[countRight] ? left[countLeft++] : right[countRight++]);
+            if (countLeft == left.length) {
+                System.arraycopy(right, countRight, rsl, ++count, right.length - countRight);
+                break;
+            }
+            if (countRight == right.length) {
+                System.arraycopy(left, countLeft, rsl, ++count, left.length - countLeft);
+                break;
+            }
+            count++;
+        }
         return rsl;
     }
 }
