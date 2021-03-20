ac1b97dffdeef16eeed928b4627f108185b0b75e
ArvikVan <arvikvan@gmail.com>
2021-03-20 23:27:21 +0300

    RepositionElement

---
+++ src/com/test/FactoryTest.java
@@ -0,0 +1,24 @@
+package com.test;
+import com.company.collections.arraylist.Factory;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FactoryTest {
+    @Test
+    public void main() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Factory.main(null);
+        String ln = System.lineSeparator();
+        String expected = "first" + ln + "second"
+                + ln + "third"
+                + ln + "fourth"
+                + ln + "fifth" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+}
