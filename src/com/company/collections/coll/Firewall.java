7c8245a335c0eec9698422199beae4602f789e0f
ArvikVan <arvikvan@gmail.com>
2021-03-24 01:01:15 +0300

    4. Собственный файерволл

---
+++ src/com/company/collections/coll/Firewall.java
@@ -0,0 +1,17 @@
+package com.company.collections.coll;
+// 4. Собственный файерволл
+//1. Если в строке нет ни одного слова из коллекции Set, то метод должен вернуть строку: Вы сделали правильный выбор!
+//2. Если в строке имеется хоть одно из слов из коллекции Set, метод должен вернуть строку: Выберите другую статью...
+import java.util.Set;
+
+public class Firewall {
+    public static String checkName(String s, Set<String> words) {
+        String[] strings = s.split(" ");
+        for (String str : strings) {
+         if (words.contains(str)) {
+             return "Выберите другую статью...";
+         }
+        }
+        return "Вы сделали правильный выбор!";
+    }
+}
--- src/com/company/collections/coll/Pangram.java
+++ src/com/company/collections/coll/Pangram.java
@@ -1,5 +1,5 @@
 package com.company.collections.coll;
-
+// 3. Панграмма
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
