4a45e08a3608491993052e4843b45c1eb7e9f86d
ArvikVan <arvikvan@gmail.com>
2021-03-29 00:16:01 +0300

    KeySet, PutIfAbsent 29-03-2021

---
+++ src/com/company/collections/map/PutIfAbsent.java
@@ -0,0 +1,49 @@
+package com.company.collections.map;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class PutIfAbsent {
+
+    private final Map<Integer, User> users;
+
+    public PutIfAbsent(Map<Integer, User> users) {
+        this.users = users;
+    }
+
+    public boolean addNewElement(User u) {
+        boolean rsl = false;
+        if (!users.containsKey(u.getId())) {
+            users.put(u.getId(), u);
+            rsl = users.containsValue(u);
+        }
+            return rsl;
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
