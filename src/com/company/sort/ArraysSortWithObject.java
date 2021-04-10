ec7199d5e5ec01cc9c77b6ec0dae0ed42c481cb4
ArvikVan <arvikvan@gmail.com>
2021-04-10 22:20:46 +0300

    2.2. Способы сортировки. Сортировка массива объектов

---
+++ src/com/company/sort/ArraysSortWithObject.java
@@ -0,0 +1,16 @@
+package com.company.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * класс описывает сортировку объектов, в данном случае по убыванию
+ * @author Arviy
+ * @version 1.0
+ */
+public class ArraysSortWithObject {
+    public static String[] sort(String[] data) {
+        Arrays.sort(data, Comparator.reverseOrder());
+        return data;
+    }
+}
