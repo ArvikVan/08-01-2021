b9d32821a1a9d37706d43d3d935ec721f4ccd6e5
ArvikVan <arvikvan@gmail.com>
2021-04-10 22:12:32 +0300

    2.1. Способы сортировки. Сортировка массива примитивов

---
+++ src/com/company/sort/ArraysSort.java
@@ -0,0 +1,15 @@
+package com.company.sort;
+/**
+ * класс описывает сортировку массива
+ * @author Arviy
+ * @version 1.0
+ */
+
+import java.util.Arrays;
+
+public class ArraysSort {
+    public static long[] sort(long[] data) {
+        Arrays.sort(data);
+        return data;
+    }
+}
--- src/com/test/ThenComparingMethodTest.java
+++ src/com/test/ThenComparingMethodTest.java
@@ -5,10 +5,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.Assert.*;
 public class ThenComparingMethodTest {
