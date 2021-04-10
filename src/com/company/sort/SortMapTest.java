bd8c9713c2a8324e252badb70761f669b7489f5d
ArvikVan <arvikvan@gmail.com>
2021-04-11 02:39:39 +0300

    2.7. Способы сортировки. Сортировка Map.

---
+++ src/com/company/sort/SortMapTest.java
@@ -0,0 +1,24 @@
+package com.company.sort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.Assert.*;
+
+public class SortMapTest {
+    @Test
+    public void test() {
+        Map<Integer, String> input = Map.of(
+                1, "1",
+                2, "2",
+                3, "3"
+        );
+        Map<Integer, String> expect = new TreeMap<>(input);
+        Map<Integer, String> result = SortMap.sort(input);
+        Assert.assertEquals(expect, result);
+    }
+
+}
