5e67ed7f4972678f1e0af7fc575e101f8ab86f9c
ArvikVan <arvikvan@gmail.com>
2021-04-17 12:20:52 +0300

    ws17-04-2021

---
+++ src/com/company/collections/arraylist/CheckerAhdGetter.java
@@ -0,0 +1,9 @@
+package com.company.collections.arraylist;
+
+import java.util.List;
+
+public class CheckerAhdGetter {
+    public static String getElement(List<String> list) {
+        return null;
+    }
+}
---
+++ src/com/company/collections/arraylist/CheckerAhdGetterTest.java
@@ -0,0 +1,12 @@
+package com.company.collections.arraylist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckerAhdGetterTest {
+
+    @Test
+    public void getElement() {
+    }
+}
