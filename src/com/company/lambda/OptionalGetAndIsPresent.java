eac7f5029e56c7ee38268ab6997e55f76f3f6741
ArvikVan <arvikvan@gmail.com>
2021-04-19 23:35:34 +0300

    5.2. Optional. Методы isPresent(), isEmpty(), get()

---
+++ src/com/company/lambda/OptionalGetAndIsPresent.java
@@ -0,0 +1,28 @@
+package com.company.lambda;
+/**
+ * 1. Написать метод indexOf(). Он ищет индекс по значению.
+ * Если индекс не найден, должно вернуться Optional.empty().
+ * Если найден, то Optional из этого индекса
+ * * 2. В методе get() нужно вызвать метод indexOf().
+ * Если вернулся пустой Optinal, то метод должен вернуть -1, в противном случае сам индекс
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.Optional;
+
+public class OptionalGetAndIsPresent {
+    public static int get(int[] data, int el) {
+        if (OptionalGetAndIsPresent.indexOf(data, el).isPresent()) {
+            return OptionalGetAndIsPresent.indexOf(data, el).get();
+        } return -1;
+    }
+
+    private static Optional<Integer> indexOf(int[] data, int el) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                return Optional.of(i);
+            }
+        } return Optional.empty();
+    }
+}
