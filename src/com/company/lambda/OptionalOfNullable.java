bfd746c06c3625a740ea027b2c2b7e89f8023fe0
ArvikVan <arvikvan@gmail.com>
2021-04-20 00:12:07 +0300

    5.4. Optional. Метод ofNullable()

---
+++ src/com/company/lambda/OptionalOfNullable.java
@@ -0,0 +1,10 @@
+package com.company.lambda;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalOfNullable {
+    public static Optional<String> findValue(List<String> strings, String value) {
+        return strings.equals(value) ? Optional.of(value) : Optional.ofNullable(value);
+    }
+}
