0feac6ea0ed00dfe573f84db0fd610fd1eadd881
ArvikVan <arvikvan@gmail.com>
2021-04-22 01:05:41 +0300

    0.1. Понятие редукции. Произвольный Collector для сборки в коллекцию

---
+++ src/com/company/stream/CollectorClass.java
@@ -0,0 +1,26 @@
+package com.company.stream;
+/**
+ * класс описывает создание коллекции и добавление элемента.
+ * Коллекцией будет LinkedList, для добавление нужно использовать метод add()
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class CollectorClass {
+    public static List<Integer> collect(List<Integer> list) {
+        Supplier<List<Integer>> supplier = () -> new LinkedList<Integer>();
+        BiConsumer<List<Integer>, Integer> biConsumer = (list1, e) -> list1.add(e);
+        BinaryOperator<List<Integer>> operator = (left, right) -> {
+            left.addAll(right);
+            return left;
+        };
+        return list.stream().collect(Collector.of(supplier, biConsumer, operator));
+    }
+}
---
+++ src/com/test/CollectorClassTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import com.company.stream.CollectorClass;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CollectorClassTest {
+
+    @Test
+    public void test() {
+        assertEquals(
+                List.of(1, 2, 3),
+                CollectorClass.collect(List.of(1, 2, 3))
+        );
+    }
+
+}
