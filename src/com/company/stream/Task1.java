e7dd51a995058ea2fc4057fc87b7ad76faaab76d
ArvikVan <arvikvan@gmail.com>
2021-04-24 23:51:40 +0300

    10.1. Удалить из числового списка все элементы, кратные 4

---
+++ src/com/company/stream/Task1.java
@@ -0,0 +1,17 @@
+package com.company.stream;
+/**
+ * Дан исходный список. Получить список без элементов, кратных 4, из исходного списка.
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task1 {
+    public static List<Integer> retain(List<Integer> list) {
+        return list.stream()
+                .filter(x -> x % 4 != 0)
+                .collect(Collectors.toList());
+    }
+}
