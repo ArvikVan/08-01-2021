26cdc0346c66eb2810bf939121a79290ff5ce58f
ArvikVan <arvikvan@gmail.com>
2021-04-24 23:58:08 +0300

    1.1. Создание стрима. Метод stream()

---
+++ src/com/company/stream/StreamMethod.java
@@ -0,0 +1,16 @@
+package com.company.stream;
+/**
+ * Ниже, приведен код. Создайте стрим из переданной коллекции
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class StreamMethod {
+    public static Stream<Integer> createStream(Collection<Integer> data) {
+        return data.stream();
+    }
+}
---
+++ src/com/company/stream/StreamMethodTest.java
@@ -0,0 +1,16 @@
+package com.company.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class StreamMethodTest {
+    @Test
+    public void whenUserStreamMethod() {
+        assertEquals(3, StreamMethod.createStream(
+                List.of(1, 2, 3)
+        ).count());
+    }
+}
