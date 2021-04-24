1cff4b0b8a4e18296fd887839b553065bc3b25c6
ArvikVan <arvikvan@gmail.com>
2021-04-24 23:28:55 +0300

    0.3. Понятие редукции. Метод reduce(). Произведение чисел

---
+++ src/com/company/stream/ReduceMethodOneArgument.java
@@ -0,0 +1,19 @@
+package com.company.stream;
+/**В Stream есть метод reduce(BinaryOperator<T> operator).
+ * Этот метод находит первый элемент стрима, он становится результатом,
+ * а потом уже начинает применять оператор к последующим элементам.
+ * Ваша задача найти произведение чисел, используя данный метод.
+ * Размер списка всегда больше 0
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.List;
+
+public class ReduceMethodOneArgument {
+    public static Integer reduce(List<Integer> list) {
+        return list.stream()
+                .reduce((n1, n2) -> n1 * n2)
+                .get();
+    }
+}
