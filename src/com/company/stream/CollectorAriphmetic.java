c625ab1f46eccb90b14e4ba1b4c89f92a74d68da
ArvikVan <arvikvan@gmail.com>
2021-04-24 23:19:17 +0300

    0.2. Понятие редукции. Произвольный Collector для подсчета

---
+++ src/com/company/stream/CollectorAriphmetic.java
@@ -0,0 +1,34 @@
+package com.company.stream;
+/**
+ * Ваша задача дописать метод, чтобы он умножал элементы списка
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class CollectorAriphmetic {
+    public static Integer collect(List<Integer> list) {
+        Supplier<List<Integer>> supplier = LinkedList::new;
+        BiConsumer<List<Integer>, Integer> consumer = List::add;
+        BinaryOperator<List<Integer>> merger = (xs, ys) -> {
+            xs.addAll(ys);
+            return xs;
+        };
+        Function<List<Integer>, Integer> function = (ns) -> {
+            int number = 1;
+            for (Integer n : ns) {
+                number = number * n;
+            }
+            return number;
+        };
+        return list.stream()
+                .collect(Collector.of(supplier, consumer, merger, function));
+    }
+}
