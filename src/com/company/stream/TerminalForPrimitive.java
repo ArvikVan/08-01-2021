b1cef4d05cf13b7d1aa4ed8de73b43861846a5cc
ArvikVan <arvikvan@gmail.com>
2021-04-25 23:18:17 +0300

    4.5. Терминальные операции. Операции стримов из примитивов

---
+++ src/com/company/stream/TerminalForPrimitive.java
@@ -0,0 +1,41 @@
+package com.company.stream;
+/**
+ * min(), max() возвращают OptionalInt, для получения элемента нужно использовать метод getAsInt()
+ *
+ * average() возвращает OptionalDouble, для получения элемента нужно использовать метод getAsDouble()
+ *
+ * Ваша задача дописать методы. Для получения стрима нужно использовать Arrays.stream()
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.Arrays;
+
+public class TerminalForPrimitive {
+    private int[] data;
+
+    public TerminalForPrimitive(int[] data) {
+        this.data = data;
+    }
+
+    public long count() {
+        return Arrays.stream(data).count();
+    }
+
+    public long max() {
+        return Arrays.stream(data).max().getAsInt();
+    }
+
+    public long min() {
+        return Arrays.stream(data).min().getAsInt();
+    }
+
+    public double avg() {
+        return Arrays.stream(data).average().getAsDouble();
+    }
+
+    public long sum() {
+        return Arrays.stream(data).sum();
+    }
+
+}
