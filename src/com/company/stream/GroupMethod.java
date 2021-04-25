b4493d05f8743db126b6d869c7ab025de96564e1
ArvikVan <arvikvan@gmail.com>
2021-04-25 10:43:21 +0300

    2.1. Группировка элементов. Группировка пользователей по возрасту

---
+++ src/com/company/stream/GroupMethod.java
@@ -0,0 +1,54 @@
+package com.company.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Класс Collectors содержит метод groupingBy(Function), его можно использовать для группировки,
+ * по значению, которое возвращает Function. Данный метод вернет мапу с типом, которая возвращает
+ * Function и значением списка по элементам стрима.
+ * задача сгруппировать пользователей по возрасту
+ * @author arvik
+ * @version 1.0
+ */
+public class GroupMethod {
+    public static class User {
+        private int age;
+        private String name;
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return age == user.age &&
+                    Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, name);
+        }
+    }
+
+    public static Map<Integer, List<User>> groupBy(List<User> users) {
+        return users.stream()
+                .collect(Collectors.groupingBy(User::getAge));
+    }
+
+}
---
+++ src/com/test/GroupMethodTest.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import com.company.stream.GroupMethod;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class GroupMethodTest {
+    @Test
+    public void test() {
+        GroupMethod.User u1 = new GroupMethod.User(10, "Ivan");
+        GroupMethod.User u2 = new GroupMethod.User(10, "Boris");
+        GroupMethod.User u3 = new GroupMethod.User(15, "Semen");
+        GroupMethod.User u4 = new GroupMethod.User(20, "Petr");
+        Map<Integer, List<GroupMethod.User>> expect = Map.of(
+                10, List.of(u1, u2),
+                15, List.of(u3),
+                20, List.of(u4)
+        );
+        assertEquals(expect, GroupMethod.groupBy(
+                List.of(u1, u2, u3, u4)
+        ));
+    }
+}
