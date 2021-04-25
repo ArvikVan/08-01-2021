24a1e5d1ecc60bc17ff38c4be33ae515d87894e9
ArvikVan <arvikvan@gmail.com>
2021-04-25 10:26:04 +0300

    1. flatMap для итератора

---
+++ src/com/company/stream/FlatIt.java
@@ -0,0 +1,28 @@
+package com.company.stream;
+/**
+ *
+* Iterator это метод, который позволяет пробежаться по элементам коллекции
+* Iterator<Integer> - это метод который пробегается по элементам коллекции содержащей Integer
+* Iterator<Iterator<Integer>> - это метод который пробегает по итераторам Iterator<Integer>
+*
+ */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class FlatIt {
+    public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
+        List<Integer> rsl = new ArrayList<>();
+        while (it.hasNext()) {
+            Iterator<Integer> iterator = it.next();
+            while (iterator.hasNext()) {
+                rsl.add(iterator.next());
+            }
+        }
+        return rsl;
+    }
+}
---
+++ src/com/test/FlatItTest.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import com.company.stream.FlatIt;
+import org.junit.Test;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class FlatItTest {
+
+    @Test
+    public void whenIt() {
+        Iterator<Iterator<Integer>> it = List.of(
+                List.of(1).iterator(),
+                List.of(2, 3).iterator()
+        ).iterator();
+        assertThat(
+                FlatIt.flatten(it),
+                is(List.of(1, 2, 3))
+        );
+    }
+}
