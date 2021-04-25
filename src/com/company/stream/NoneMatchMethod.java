60509c981546779ee7ae582c6ff27aeb329b0b6f
ArvikVan <arvikvan@gmail.com>
2021-04-25 23:12:25 +0300

    4.4. Терминальные операции. Метод noneMatch()

---
+++ src/com/company/stream/NoneMatchMethod.java
@@ -0,0 +1,9 @@
+package com.company.stream;
+
+import java.util.List;
+
+public class NoneMatchMethod {
+    public static boolean check(List<String> list) {
+        return list.stream().noneMatch(n -> n.length() == 0);
+    }
+}
---
+++ src/com/test/NoneMatchMethodTest.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import com.company.stream.NoneMatchMethod;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class NoneMatchMethodTest {
+    @Test
+    public void test() {
+        assertTrue(NoneMatchMethod.check(List.of("1", "2", "3")));
+        assertFalse(NoneMatchMethod.check(List.of("1", "", "3")));
+    }
+}
