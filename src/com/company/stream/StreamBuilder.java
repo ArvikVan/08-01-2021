a271b7b43812ed6248c16b9a2ba3998336ec2257
ArvikVan <arvikvan@gmail.com>
2021-04-25 09:51:27 +0300

    1.5. Создание стрима. Метод Stream.builder()

---
+++ src/com/company/stream/StreamBuilder.java
@@ -0,0 +1,24 @@
+package com.company.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * Иногда нужно создать стрим выборочно из нескольких элементов.
+ * Для этого бывает полезно использовать метод Stream.builder(),
+ * он нам возвращает объект для построения стрима.
+ * У этого объекта мы можем вызвать метод add() для добавления
+ * элемента в стрим и build() для окончательного построения стрима.
+ * Пример,
+ * Stream<Object> stream = Stream.builder().add(1).add(2).build()
+ * Ваша задача создать стрим из первого и последнего элементов массива
+ * @author arvik
+ * @version 1.0
+ */
+public class StreamBuilder {
+    public static Stream<Object> createStream(Integer[] data) {
+        return Stream.builder()
+                .add(data[0])
+                .add(data[data.length - 1])
+                .build();
+    }
+}
