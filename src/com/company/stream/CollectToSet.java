e46de6241b697b7c1b9560a2f907ca5a0d21a354
ArvikVan <arvikvan@gmail.com>
2021-04-25 19:37:53 +0300

    2.2. Сборка элементов. Сборка в Set

---
+++ src/com/company/stream/CollectToSet.java
@@ -0,0 +1,20 @@
+package com.company.stream;
+/**
+ * Для того чтобы собрать данные в сет, мы можем использовать методы collect() и Collectors.toSet().
+ * Сборка идет в HashSet
+ * * Например,
+ * * Set<Integer> result = Set.of(1, 2).stream().collect(Collectors.toSet());
+ * * Ваша задача собрать, переданный стрим в сет
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectToSet {
+    public static Set<Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toSet());
+    }
+}
