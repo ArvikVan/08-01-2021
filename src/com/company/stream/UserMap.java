cfdf7d2c33a7bec52ba631ba161e65075d0d1892
ArvikVan <arvikvan@gmail.com>
2021-04-26 00:02:54 +0300

    5.3. Промежуточные операции. Метод map(). Map с объектами

---
+++ src/com/company/stream/UserMap.java
@@ -0,0 +1,21 @@
+package com.company.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserMap {
+    public static class User{
+        private int age;
+
+        public User(int age) {
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+    public static List<User> map(List<Integer> ages) {
+        return ages.stream().map(x -> new User(x)).collect(Collectors.toList());
+    }
+}
---
+++ src/com/test/UserMapTest.java
@@ -0,0 +1,26 @@
+package com.test;
+/**
+ * Дан список возрастов. Вам нужно создать список пользователей с такими возрастами
+ * @author arviy
+ * @version 1.0
+ */
+
+import com.company.stream.UserMap;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class UserMapTest {
+
+    @Test
+    public void test() {
+        List<Integer> ages = List.of(1, 2);
+        List<UserMap.User> result = UserMap.map(ages);
+        Iterator<UserMap.User> iterator = result.iterator();
+        assertEquals(1, iterator.next().getAge());
+        assertEquals(2, iterator.next().getAge());
+    }
+}
