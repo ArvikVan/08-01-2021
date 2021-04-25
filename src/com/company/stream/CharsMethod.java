4d780ea9c31d7e55c49c3541bf060685466df880
ArvikVan <arvikvan@gmail.com>
2021-04-26 00:17:44 +0300

    5.4. Промежуточные операции. Метод mapToObj(). Стрим из символов строки

---
+++ src/com/company/stream/CharsMethod.java
@@ -0,0 +1,23 @@
+package com.company.stream;
+/**
+ * map() возвращает стрим, того же типа (в смысле стрим объектов или стрим примитивов).
+ * Если мы вызовем map() для стрима объектов, мы получим стрим из объектов.
+ * Если мы вызовем map() для стрима примитивов IntStream, мы получим стрим IntStream.
+ *
+ * Если нам нужно преобразовать стрим примитивов в стрим объектов,
+ * мы можем использовать метод mapToObj(), работает он так же как и map()
+ * Из символов строки можно получим стрим, используя метод chars() в класс String.
+ * Но этот стрим возвращает нам IntStream. Ваша задача преобразовать стрим, который вернет chars(),
+ * в стрим Charater и собрать в список.
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CharsMethod {
+    public static List<Character> symbols(String string) {
+        return string.chars().mapToObj(x -> (char) x).collect(Collectors.toList());
+    }
+}
