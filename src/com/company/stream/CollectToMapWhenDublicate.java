c8edc0c3a9fbcc11ea42e363376b92c1861d8e43
ArvikVan <arvikvan@gmail.com>
2021-04-25 22:33:50 +0300

    2.8. Сборка элементов. Сборка в Map, когда есть дубликаты

---
+++ src/com/company/stream/CollectToMapWhenDublicate.java
@@ -0,0 +1,24 @@
+package com.company.stream;
+/**
+ * Метод, который был показан ранее нельзя использовать,
+ * если стрим содержит дубликаты. Для того чтобы собрать данные в мапу в этом случае,
+ * мы можем использовать методы collect() и Collectors.toMap(), но перегруженный. Сборка идет в HashMap
+ * 1 аргумент - ключ
+ * 2 аргумент - значение
+ * 3 аргумент - указываем, что делать в случае дубликата ключа, оставим мы старое значение или новое
+ * В примере, ключом и значением будет сам элемент, а в случае дубликата мы указываем, что останется предыдущее значение
+ * собрать, переданный стрим чисел в мапу, где ключ сам элемент,
+ * а значение это его квадрат. Нужно использовать версию toMap() описанную в задании.
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectToMapWhenDublicate {
+    public static Map<Integer, Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toMap(k -> k, v -> v * v, (prev, curr) -> prev));
+    }
+}
---
+++ src/com/test/CollectToMapWhenDublicateTest.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import com.company.stream.CollectToMapWhenDublicate;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class CollectToMapWhenDublicateTest {
+    @Test
+    public void test() {
+        List<Integer> data = List.of(2, 3, 3);
+        Map<Integer, Integer> expect = Map.of(
+                2, 4,
+                3, 9
+        );
+        Map<Integer, Integer> result = CollectToMapWhenDublicate.collect(data.stream());
+        assertEquals(expect, result);
+    }
+}
