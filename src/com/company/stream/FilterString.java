d9e21a3e36657e4d6382210508ccb9100a845d06
ArvikVan <arvikvan@gmail.com>
2021-04-27 00:47:35 +0300

    6.2. Промежуточные операции. Метод filter(). Фильтрация строк

---
+++ src/com/company/stream/FilterString.java
@@ -0,0 +1,19 @@
+package com.company.stream;
+/**
+ * Если условие громоздкое, то можно расположить несколько вызовов filter подряд.
+ * Ваша задача отфильтровать список строк по следующему признаку.
+ * Строка должна начинаться на "A" и заканчиваться на ".java"
+ *
+ */
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterString {
+    public static List<String> filter(List<String> data) {
+        return data.stream()
+                .filter(x -> x.startsWith("A"))
+                .filter(x -> x.endsWith(".java"))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/com/test/FilterStringTest.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import com.company.stream.FilterString;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FilterStringTest {
+
+    @Test
+    public void test() {
+        List<String> list = List.of("Abc.java", "abc.java", "Abc");
+        assertEquals(List.of("Abc.java"), FilterString.filter(list));
+    }
+}
