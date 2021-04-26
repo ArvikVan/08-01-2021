3bc13977c7ac0bf9c60751f600994f807f01cb96
ArvikVan <arvikvan@gmail.com>
2021-04-27 00:12:58 +0300

    5.6. Промежуточные операции. Метод boxed().

---
+++ src/com/company/stream/BoxedMethod.java
@@ -0,0 +1,16 @@
+package com.company.stream;
+/**
+ * Если у нас стрим из примитивов, а мы хотим получим стрим из объектов
+ * оберток этого примитива, то мы можем использовать метод boxed()
+ */
+
+import javax.swing.event.InternalFrameEvent;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BoxedMethod {
+    public static List<Integer> boxed(int[] data) {
+            return Arrays.stream(data).boxed().collect(Collectors.toList());
+    }
+}
