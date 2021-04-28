1241378d2320ec3c6bd042385f7beeec20f0f4b7
ArvikVan <arvikvan@gmail.com>
2021-04-29 00:05:56 +0300

    9.6. Optional и Stream. Метод flatMap()

---
+++ src/com/company/stream/OptionalFlatMap.java
@@ -0,0 +1,21 @@
+package com.company.stream;
+/**
+ * Ваша задача найти первую строку, которая кончается на ".java" и вернуть Optional из длины этой строки
+ * @author arviy
+ * @version 1.0
+ */
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OptionalFlatMap {
+    public static Optional<Integer> flatMap(List<String> strings) {
+        return strings.stream()
+                .findFirst()/**
+         получаем элемент из стрима, если пусто то вернем пустой Optional
+         если есть то возвращаем нулевой элемент Optional**/
+                .filter(n -> n.endsWith(".java"))// фтильтрууем по ".java"
+                .flatMap(x -> Optional.of(x.length())); //возвращаем Optional из длины строки
+    }
+}
---
+++ src/com/test/OptionalFlatMapTest.java
@@ -0,0 +1,32 @@
+package com.test;
+import com.company.stream.OptionalFlatMap;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class OptionalFlatMapTest {
+
+    @Test
+    public void whenExist() {
+        assertEquals(
+                Optional.of("Hello.java".length()),
+                OptionalFlatMap.flatMap(
+                        List.of("Hello.java", "Hello.js", "Hello.py")
+                )
+        );
+    }
+
+    @Test
+    public void whenNotExist() {
+        assertEquals(
+                Optional.empty(),
+                OptionalFlatMap.flatMap(
+                        List.of("Hello.cpp", "Hello.js", "Hello.py")
+                )
+        );
+    }
+
+}
