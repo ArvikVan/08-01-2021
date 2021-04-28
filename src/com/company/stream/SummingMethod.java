1e994397364d06ed8352d1c36e116945454cd5ad
ArvikVan <arvikvan@gmail.com>
2021-04-29 00:51:37 +0300

    lastOne

---
+++ src/com/company/stream/SummingMethod.java
@@ -0,0 +1,70 @@
+package com.company.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SummingMethod {
+    public static class User {
+
+        private String name;
+
+        private List<Bill> bills = new ArrayList<>();
+
+        public User(String name, List<Bill> bills) {
+            this.name = name;
+            this.bills = bills;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<Bill> getBills() {
+            return bills;
+        }
+    }
+
+    public static class Bill {
+
+        private int balance;
+
+        public Bill(int balance) {
+            this.balance = balance;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+    }
+
+    private static class Pair {
+        private User user;
+        private Bill bill;
+
+        public Pair(User user, Bill bill) {
+            this.user = user;
+            this.bill = bill;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public Bill getBill() {
+            return bill;
+        }
+    }
+
+    public static Map<String, Integer> summing(List<User> users) {
+        return users.stream()
+                .collect(Collectors.groupingBy(x -> x.getName(),
+                        Collectors.summingInt(x -> x.getBills()
+                        .stream()
+                        .mapToInt(w -> w.getBalance())
+                        .sum())));
+    }
+
+
+}
---
+++ src/com/test/SummingMethodTest.java
@@ -0,0 +1,32 @@
+package com.test;
+import com.company.stream.SummingMethod;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static com.company.stream.SummingMethod.*;
+
+public class SummingMethodTest {
+
+    @Test
+    public void test() {
+        Bill b1 = new Bill(1);
+        Bill b2 = new Bill(2);
+        Bill b3 = new Bill(3);
+        Bill b4 = new Bill(4);
+        Bill b5 = new Bill(5);
+        Bill b6 = new Bill(6);
+        User u1 = new User("u1", List.of(b1));
+        User u2 = new User("u2", List.of(b2, b3));
+        User u3 = new User("u3", List.of(b4, b5, b6));
+        Map<String, Integer> expect = Map.of(
+                "u1", 1,
+                "u2", 5,
+                "u3", 15
+        );
+        assertEquals(expect, SummingMethod.summing(List.of(u1, u2, u3)));
+    }
+
+}
