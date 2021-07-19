b2af6983e7a95a68c687b44b5b91324840c8c721
ArvikVan <arvikvan@gmail.com>
2021-07-19 22:53:13 +0300

    19-07-2021 prepToExam DirectoryStream.Filter 1.1

--- src/main/java/nio/DirList.java
+++ src/main/java/nio/DirList.java
@@ -7,17 +7,24 @@ import java.nio.file.attribute.BasicFileAttributes;
 /**
  * класс описывает вывод сожержимого каталога
  * @author arvik
- * @version 1.0
+ * @version 1.1
  * содаем стрингу в которой будет адрес каталога
  * получаем и обрабатываем поток ввода каталога try with resourses
  * указываем на то в каком каталоге находимся
  * выводим содержимое каталога циклом, т.к. DyrectoreStream реализует интерфейс Iterable
  * в параметрах метода getName указываем индекс того каталога который надо показать
+ * 1.1 добавялем фильтр
  */
 public class DirList {
     public static void main(String[] args) {
         String dirname = "src/main/java/nio";
-        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(dirname))) {
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path entry) throws IOException {
+                return Files.isWritable(entry);
+            }
+        };
+                try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(dirname), filter)) {
             System.out.println("Каталог в котором находимся: " + dirname);
             for (Path path : directoryStream) {
                 BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
