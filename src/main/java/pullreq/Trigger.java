46b9c7cbe2daca0afe4cdc8f80ae585e00632bbd
ArvikVan <arvikvan@gmail.com>
2021-12-09 20:08:37 +0300

    newBranchForPullREq

---
+++ src/main/java/pullreq/Trigger.java
@@ -0,0 +1,13 @@
+package pullreq;
+
+/**
+ * @author arvikv
+ * @version 1.0
+ * @since 09.12.2021
+ */
+public class Trigger {
+    public static void main(String[] args) {
+        String name = "Govno";
+        System.out.println(name);
+    }
+}
