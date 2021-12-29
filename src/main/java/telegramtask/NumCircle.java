7375ad3e44451e5b7881c2e806a32a4a17369f3f
ArvikVan <arvikvan@gmail.com>
2021-12-30 00:07:24 +0300

    num++Example

--- src/main/java/telegramtask/NumCircle.java
+++ src/main/java/telegramtask/NumCircle.java
@@ -6,10 +6,15 @@ package telegramtask;
  * @since 29.12.2021
  */
 public class NumCircle {
+    /**
+     * получаем от 1 до 100 (num = num + 1;)
+     * получаем 0, 100 раз (num = num++;)
+     * @param args args
+     */
     public static void main(String[] args) {
         int num = 0;
         for (int i = 0; i < 100; i++) {
-            num = num + 1;
+            num = num++;
             System.out.println(num);
         }
     }
