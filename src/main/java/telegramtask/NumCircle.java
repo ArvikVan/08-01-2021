fe5ef0bf74d3ce3dda68f4c36f86205391952762
ArvikVan <arvikvan@gmail.com>
2021-12-29 23:59:25 +0300

    telegramTrainie

---
+++ src/main/java/telegramtask/NumCircle.java
@@ -0,0 +1,16 @@
+package telegramtask;
+
+/**
+ * @author arvikv
+ * @version 1.0
+ * @since 29.12.2021
+ */
+public class NumCircle {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i < 100; i++) {
+            num = num + 1;
+            System.out.println(num);
+        }
+    }
+}
