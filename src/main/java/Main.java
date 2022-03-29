144c09ffedc495341e3c47dc204b43aaa8787143
ArvikVan <arvikvan@gmail.com>
2022-03-29 22:21:43 +0300

    farsh_

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        int d = 1212;
+       /* int d = 1212;
         System.out.println("Стук " + d);
-        System.out.println("Ping");
+        System.out.println("Ping");*/
+        equalsNum("13.6", "13.3");
+    }
+
+    public static int equalsNum(String firstNum, String secondNum) {
+        int res = 0;
+        Double first = Double.parseDouble(firstNum);
+        Double sec = Double.parseDouble(secondNum);
+        System.out.println(first);
+        System.out.println(sec);
+        if (first < sec) {
+            res = -1;
+        }
+        if (first > sec) {
+            res = 1;
+        }
+        if (first.equals(sec)) {
+            res = 0;
+        }
+        System.out.println(res);
+        return res;
     }
 }
