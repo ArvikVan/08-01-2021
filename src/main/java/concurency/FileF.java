970fd5aeed93cd21c46e03845debd1b5994168b2
khubetsov.ay <khubetsov.ay@tr-media.ru>
2023-04-08 22:13:32 +0300

    docker compose postgres

--- src/main/java/concurency/FileF.java
+++ src/main/java/concurency/FileF.java
@@ -5,14 +5,25 @@ import java.io.IOException;
 
 public class FileF {
     public static void main(String[] args) throws IOException {
-        File file = new File("/mnt/v-sql9/CrowdSourcing/templates/actTemplate.docx");
-        System.out.println(file.getAbsolutePath());
-        System.out.println(file.getCanonicalPath());
-        System.out.println(file.getPath());
-        if (file.exists()) {
-            System.out.println("ВСе должно быть норм");
-        } else {
-            System.out.println("файл не найден");
+//        File file = new File("/mnt/v-sql9/CrowdSourcing/templates/actTemplate.docx");
+//        System.out.println(file.getAbsolutePath());
+//        System.out.println(file.getCanonicalPath());
+//        System.out.println(file.getPath());
+//        if (file.exists()) {
+//            System.out.println("ВСе должно быть норм");
+//        } else {
+//            System.out.println("файл не найден");
+//        }
+        String name = "Bran";
+        System.out.println(func(name));
+    }
+    public static String func(String ctr) {
+        int i = 0;
+        String result = "";
+        while (i < ctr.length()) {
+            result = ctr.charAt(i) + result;
+            i = i +1;
         }
+        return result;
     }
 }
